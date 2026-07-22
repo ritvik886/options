165f81eb5c20ab7068d3fd5e7a2fb2b2b68b05e5
ritvik886 <ritviktaparia123@gmail.com>
2026-07-22 15:05:54 -0500

    .

--- options.py
+++ options.py
@@ -9,13 +9,15 @@ import plotly.graph_objects as go
 
 warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Unusual Options Activity Scanner", layout="wide")
-# Hide the Streamlit menu and footer to make it look professional
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        </style>
-        """
+st.set_page_config(
+    page_title="Unusual Options Activity Scanner",
+    layout="wide",
+    menu_items={
+        'Get help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 # --- CONFIGURATION ---
 DEFAULT_TICKERS = "AAPL, NVDA, TSLA"
